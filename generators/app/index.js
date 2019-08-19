669ba78816d2fadd4400c2b6207883d718d6bf98
blascone <blascone@blascone.it>
2019-08-19 15:36:46 +0200

    gitignore update and index.js update

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,19 +1,42 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  method1() {
-    this.log('method 1 just ran');
-  }
 
-  method2() {
-    this.log('method 2 just ran');
-  }
+ async prompting() {
+    const self = this;
+    self.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Project description",
+        default: this.appname + " web application"
+      }, 
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license should be used?',
+        choices: ['UNLICENSED', 'MIT'],
+        default: 'MIT',
+      }
+    ]);
 
+    this.log("app name", self.answers.name);
+    this.log("app description", self.answers.description);
+    this.log("app license", self.answers.license);
+
+ }
 
  writing() {
+
     this.fs.copy(
       this.templatePath('testfolder'),
-      this.destinationPath('testfolder')
+      this.destinationPath(this.answers.name)
     );
   }
 
